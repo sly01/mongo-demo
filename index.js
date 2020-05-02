34e33b2cd162325af0a056e0a3640965d95a604f
sly01 <aerkoc01@gmail.com>
2020-05-02 11:21:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connect to MongoDB...'));
