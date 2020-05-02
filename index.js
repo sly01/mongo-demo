a3d97717d841956dd0d3f170b393fc5bc8bf6197
sly01 <aerkoc01@gmail.com>
2020-05-02 20:33:49 +0200

    Updating and removing example

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function getCourses() {
         // .limit(pageSize)
         .limit(10)
         .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
+        .select({ name: 1, tags: 1, isPublished: 1, author: 1 });
     // const courses = await Course
     //     // .find({ price: { $gte: 10, $lte: 20 } })
     //     //.find({ price: { $in: [10, 15, 20] } })
@@ -59,5 +59,34 @@ async function getCourses() {
     console.log(courses);
 }
 
+async function updateCourse(id) {
+    // Update a document(query first)
+    // const course = await Course.findById(id);
+    // if (!course) return;
+    // course.set({
+    //     name: 'Linux System 1 update'
+    // })
+    // course.save();
+
+    // Update a document(update first)
+    // const result = await Course.update({ _id: id }, {
+    //     $set: { name: 'Linux System 1' }
+    // });
+
+    // Update a document(update first) and return it
+    const result = await Course.findByIdAndUpdate({ _id: id }, {
+        $set: { name: 'Linux System 1 update' }
+    }, { new: true });
+}
+
+async function removeCourse(id) {
+    // const result = await Course.deleteOne({ _id: id});
+    // const result = await Course.deleteMany({_id: id});
+    const course = await Course.findByIdAndRemove(id);
+    console.log(course);
+}
+
+//updateCourse('5ead3f260552194ce48ce4ec');
+//removeCourse('5ead3f260552194ce48ce4ec');
 getCourses();
 // createCourse();
